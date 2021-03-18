2ec7d682714ccc09f0ad1366aade1303e070c655
a-k-r-a-k-r <akr_akr@protonmail.com>
2021-03-18 21:01:46 +0530

    3rd script added

---
+++ script_3/script_3.py
@@ -0,0 +1,32 @@
+#program to exploit the system resources
+
+
+import tkinter
+import random
+
+root= tkinter.Tk()
+root.title("Hacked")
+root.geometry("300x100")
+root.config(bg="black")
+
+def message_show():
+    main_message = tkinter.Toplevel(root,bg="black")
+    main_message.geometry("1000x200")
+
+#change for loop with a while true statement in real scenerio
+#sometimes while may not work. therefore increase the range
+def hack():
+    for i in range(1000):
+        rand_x = random.randint(0,2000)
+        rand_y = random.randint(0,1500)
+        topj=tkinter.Toplevel(root,bg="green")
+        topj.geometry("300x100+{}+{}".format(rand_x,rand_y))
+        
+    
+
+
+
+start_btn = tkinter.Button(root,bg="green",fg="black",text="Start",command=hack)
+start_btn.pack(ipadx=30,ipady=10,padx=100,pady=20)
+
+root.mainloop()
